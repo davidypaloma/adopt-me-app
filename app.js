402ba3c436797b181ca4c1f458b2e2b7a113c7f4
David Reguart <reguartdavid@gmail.com>
2023-02-25 09:14:23 +0100

    Tailwind add

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.set('view engine', 'hbs')
 //Middleware
 app.use(express.urlencoded({ extended: false }))
 app.use(logger('dev'))
+app.use(express.static(`${__dirname}/public`))
 
 app.use(session)
 app.use(loadSessionProtSociety)
